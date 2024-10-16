3d8be2c2b8ce2ad1c581291336dd5bb42ca14187
CVisioner <yanconglin@users.noreply.github.com>
2024-10-16 09:06:40 +0200

    Update HT.py

--- ht-lcnn/lcnn/models/HT.py
+++ ht-lcnn/lcnn/models/HT.py
@@ -4,7 +4,7 @@ Deep-Hough-Transform-Line-Priors (ECCV 2020) https://arxiv.org/abs/2007.09493
 
 Yancong Lin, and Silvia Laura Pintea, and Jan C. van Gemert
 
-e-mail: y.lin-1ATtudelftDOTnl
+e-mail: yancong.linATtudelftDOTnl
 
 Vision Lab, Delft University of Technology
 
